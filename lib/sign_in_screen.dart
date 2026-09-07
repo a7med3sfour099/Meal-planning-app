82d32cc1d48dfd02b8c86050a5a0844e4034b8e3
Shahd Mohamed Farouk <shahdmohamedfarouk1112@gmail.com>
2026-09-07 07:40:27 +0300

    Add sign in and sign up screens

---
+++ lib/sign_in_screen.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  static const String correctEmail = 'shahdmohamedfarouk1112@gmail.com';
+  static const String correctPassword = 'Shahd@123';
+
+  static String userEmail = '';
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void _handleSignIn() {
+    try {
+      String email = emailController.text.trim();
+      String password = passwordController.text.trim();
+
+      if (email == SignInScreen.correctEmail && password == SignInScreen.correctPassword) {
+        SignInScreen.userEmail = email;
+        print('Welcome User');
+      } else {
+        print('Email or password is wrong');
+      }
+    } catch (e) {
+      print('Something went wrong: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/food_bg.jpg',fit: BoxFit.cover,),
+
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.15),
+                  Colors.black.withOpacity(0.75),
+                  Colors.black.withOpacity(0.9),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.0, 0.6, 1.0],
+              ),
+            ),
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Sign in ',
+                        style: GoogleFonts.notoSans(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                      Text(
+                        'Now',
+                        style: GoogleFonts.notoSans(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 6),
+                  Text(
+                    'Start planning your meals today.',
+                    style: GoogleFonts.notoSans(color: Colors.white70, fontSize: 15),
+                  ),
+                  SizedBox(height: 28),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 16),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.15),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: emailController,
+                      style: GoogleFonts.notoSans(color: Colors.white, fontSize: 16,fontWeight: FontWeight.w300),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email Address',
+                        hintStyle: TextStyle(color: Colors.white),
+                        icon: Icon(Icons.email_outlined, color: Colors.white, size: 20),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(bottom: 16),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.16),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      style: GoogleFonts.notoSans(color: Colors.white, fontSize: 16,fontWeight: FontWeight.w300),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.white),
+                        icon: Icon(Icons.lock_outline, color: Colors.white, size: 20),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: OutlinedButton(
+                      onPressed: _handleSignIn,
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.white, width: 1.5),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                      ),
+                      child: Text(
+                        'SIGN IN',
+                        style: GoogleFonts.notoSans(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 24),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(context, '/signup');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                        "Don't have an account? ",
+                              style: GoogleFonts.notoSans(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Text(
+                              'Join Us',
+                              style: GoogleFonts.notoSans(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
